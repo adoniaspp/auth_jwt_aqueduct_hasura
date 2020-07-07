db3bbcda22136f4e8bca75a7971079f82d643631
Adonias Pires <adoniaspp@gmail.com>
2020-07-07 01:12:45 -0300

    refresh_token

--- lib/controller/auth_jwt_signup_controller.dart
+++ lib/controller/auth_jwt_signup_controller.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:cryptography/cryptography.dart';
 import 'package:corsac_jwt/corsac_jwt.dart';
 import 'package:http/http.dart' as http;
 import '../auth_jwt_project.dart';
+
 
 class AuthJwtSignupController extends ResourceController {
   AuthJwtSignupController(this.context);
@@ -11,9 +13,12 @@ class AuthJwtSignupController extends ResourceController {
 
   @Operation.post()
   Future<Response> signup() async {
+    
     //Obtem os dados do body
     final bodyMap = request.body.as();
-    final password = bodyMap["input"]["senha"].toString();
+    final password = bodyMap["input"]["password"].toString();
+    final user = bodyMap["input"]["user"].toString();
+    final idPhone = bodyMap["input"]["id_phone"].toString();
 
     //Criptografa a senha
     final pbkdf2 = Pbkdf2(
@@ -21,20 +26,26 @@ class AuthJwtSignupController extends ResourceController {
       iterations: 100000,
       bits: 128,
     );
-    final nonce = Nonce.randomBytes(32);
+    final salt = Nonce.randomBytes(32);
     final hashPassword =
-        await pbkdf2.deriveBits(utf8.encode(password), nonce: nonce);
-    
+        await pbkdf2.deriveBits(utf8.encode(password), nonce: salt);
+
+    //Gera o refresh_token
+    final Random _random = Random.secure();
+    final values = List<int>.generate(32, (i) => _random.nextInt(256));
+    final refreshToken = await blake2s.hash(
+      values,
+    );
+
     //Gera o body do Hasura Graphql
-    const hasuraOperation =
-        '''mutation MyMutation(\$senha: String!, \$usuario: String!) 
-            {insert_user_teste_one(object: {senha: \$senha, usuario: \$usuario}) 
-            {id}
-        }''';
-    final variables = {
-      "usuario": bodyMap["input"]["usuario"].toString(),
-      "senha": hashPassword.toString()
-    };
+    const hasuraOperation = '''
+        mutation MyMutation(\$id_phone: String!, \$password: String!, \$user: String!, \$salt: String!, \$refresh_token: String!, \$creation_date: timestamptz!) {
+          insert_user_one(object: {user: \$user, password: \$password, id_phone: \$id_phone, refresh_token: \$refresh_token, salt: \$salt, creation_date: \$creation_date}) {
+            id
+          }
+        }       
+        ''';
+    final variables = {"user": user, "password": hashPassword.toString(), "id_phone": idPhone, "salt": salt.bytes.toString(), "creation_date": DateTime.now().toString(), "refresh_token": refreshToken.bytes.toString()};
     final bodyHasura =
         json.encode({"query": hasuraOperation, "variables": variables});
 
@@ -47,7 +58,7 @@ class AuthJwtSignupController extends ResourceController {
         body: bodyHasura);
 
     final bodyResponse = json.decode(response.body);
-    final id = bodyResponse["data"]["insert_user_teste_one"]["id"];
+    final id = bodyResponse["data"]["insert_user_one"]["id"];
 
     //Gera o token jwt
     final builder = JWTBuilder();
@@ -68,6 +79,6 @@ class AuthJwtSignupController extends ResourceController {
     print(signedToken); // prints encoded JWT
 
     //Retorna o token
-    return Response.ok(bodyResponse["data"]["insert_user_teste_one"]);
+    return Response.ok(bodyResponse["data"]["insert_user_one"]);
   }
 }
